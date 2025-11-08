40858ea2bda3e3624e003bd431c7bf1aa0ef614d
Brecht De Roover <brecht.dr36@gmail.com>
2025-11-08 10:49:06 +0100

    All data csv readed

--- global.R
+++ global.R
@@ -14,5 +14,11 @@ source("./R/helper_functions.R")
 
 # Load and prepare data once
 company_data <- read.csv("data/raw/Companies.csv")
+company_contacts_data <- read.csv("data/raw/Company_Contacts.csv")
+project_board_data <- read.csv("data/raw/Project_Board.csv")
+projects_data <- read.csv("data/raw/Projects.csv")
+research_groups_data <- read.csv("data/raw/Research_Groups.csv")
+research_participation_data <- read.csv("data/raw/Research_Participation.csv")
+researchers_data <- read.csv("data/raw/Researchers.csv")
 # sales_data <- read.csv("data/processed/sales_clean.csv") %>%
 #   mutate(date = as.Date(date))
