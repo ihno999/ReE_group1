0e07d8ef8da2fe977d36125c2aaf709d96514c01
Ihno Van de Sande <ihno.vandesande@student.ucll.be>
2025-11-08 21:41:39 +0100

    commented out ui.R and server.R

--- server.R
+++ server.R
@@ -1,8 +1,8 @@
-source("./global.R")
+# source("./global.R")
 
-server <- function(input, output) {
-  output$companies_table <- DT::renderDT({
-    company_data %>%
-      DT::datatable()
-  })
-}
+# server <- function(input, output) {
+#   output$companies_table <- DT::renderDT({
+#     company_data %>%
+#       DT::datatable()
+#   })
+# }
--- ui.R
+++ ui.R
@@ -1,20 +1,20 @@
-source("./global.R")
+# source("./global.R")
 
-ui <- fluidPage(
-  # titlePanel("Hello World!!"),
-  # sidebarLayout(
-  #   sidebarPanel(sliderInput('nb_bins', 'Number of bins', 5, 10 , 5)),
-  #   mainPanel(DT::DTOutput("companies_table"))
-  # )
+# ui <- fluidPage(
+#   # titlePanel("Hello World!!"),
+#   # sidebarLayout(
+#   #   sidebarPanel(sliderInput('nb_bins', 'Number of bins', 5, 10 , 5)),
+#   #   mainPanel(DT::DTOutput("companies_table"))
+#   # )
 
-  titlePanel("Research Collaboration Network"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("researcher", "Select Researcher:", choices = unique(researchers_data$researcher_name)),
-      selectInput("selected_field", "Field of new project", choices = NULL)
-    ),
-    mainPanel(
-      visNetworkOutput("network_plot", height = "600px")
-    )
-  )
-)
+#   titlePanel("Research Collaboration Network"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput("researcher", "Select Researcher:", choices = unique(researchers_data$researcher_name)),
+#       selectInput("selected_field", "Field of new project", choices = NULL)
+#     ),
+#     mainPanel(
+#       visNetworkOutput("network_plot", height = "600px")
+#     )
+#   )
+# )
